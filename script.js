542530ecfcb4ec5cea21f865860d9d5abad8ce00
Kiril <kpanar82@gmail.com>
2018-02-05 13:41:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+/*var sum=0;
+var number = 1;
+while(number <= 100){
+	sum +=number;
+	number++;
+
+}
+document.write(sum);*/
+
+/*var sum =1;
+while(sum<=100){
+	if(sum % 3 == 0){
+		document.write(sum + "<br>");
+		
+}
+	sum++;
+}*/
+
+
+
+
+
+
+
+
